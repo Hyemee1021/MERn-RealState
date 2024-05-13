7f1d834852a6c51f1e63ee6109c2fee18cabceda
Hye Mee Park <hyemee1021@gmail.com>
2024-05-13 10:25:18 +1000

    create user model

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
 
+//I need apllication string
+mongoose.connect(
+  "mongodb+srv://hyemee1021:JangP9941!@mern-realestate.9npdhyd.mongodb.net/?retryWrites=true&w=majority&appName=mern-realestate"
+);
 const app = express();
 
 app.listen(3000, () => {
