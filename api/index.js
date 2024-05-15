d6634b59f905acba700a771b06090ddc2611a8b2
Hye Mee Park <hyemee1021@gmail.com>
2024-05-15 20:05:03 +1000

    complete sign up functionality

--- api/index.js
+++ api/index.js
@@ -3,16 +3,22 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cors from "cors";
 dotenv.config();
 
 //I need apllication string
 mongoose.connect(
   "mongodb+srv://hyemee1021:JangP9941!@mern-realestate.9npdhyd.mongodb.net/?retryWrites=true&w=majority&appName=mern-realestate"
 );
+
 const app = express();
 //allowing json form as input
 app.use(express.json());
 
+const corsOptions = {
+  origin: true,
+};
+app.use(cors(corsOptions));
 app.listen(3000, () => {
   console.log(`Server is running on port 3000!!`);
 });
