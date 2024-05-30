c3bfc129270924e0b3ff4b2b82f44a281d1fc87d
Hye Mee Park <hyemee1021@gmail.com>
2024-05-30 21:16:52 +1000

    create listing api route and listing model in data

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -37,6 +38,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 //middleware-I use it in auth.controller
 app.use((err, req, res, next) => {
