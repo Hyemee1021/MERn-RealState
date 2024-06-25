16b4ad599f7ea55a5d4f421f2bfc7266d8775207
Hye Mee Park <hyemee1021@gmail.com>
2024-06-25 23:54:50 +1000

    creating listing functionality on process

--- clinet/src/pages/CreateListing.jsx
+++ clinet/src/pages/CreateListing.jsx
@@ -151,6 +151,12 @@ export const CreateListing = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      if (formData.imageUrls.length < 1)
+        return setError("You must upload at least one image.");
+      //+ will make data to nnumber before doing any add
+      if (+formData.regularPrice < +formData.discountPrice);
+      return setError("Discount price must be lower than regular price");
+
       setLoading(true);
       setError(false);
 
