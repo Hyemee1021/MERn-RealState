7534f27c9d24ff9986fd99251c8eec30414dc0fd
Hye Mee Park <hyemee1021@gmail.com>
2024-08-15 21:15:55 +1000

    show listing page on progress

--- clinet/src/components/ListingItem.jsx
+++ clinet/src/components/ListingItem.jsx
@@ -21,6 +21,25 @@ export const ListingItem = ({ listing }) => {
           <p className="text-sm text-gray-600 line-clamp-2">
             {listing.description}
           </p>
+          <p className="text-slate-700 font-bold">
+            $
+            {listing.offer
+              ? listing.discountPrice.toLocaleString("en-US")
+              : listing.regularPrice.toLocaleString("en-US")}
+            {listing.type === "rent" && "/month"}
+          </p>
+          <div className="text-slate-700 flex gap-4">
+            <div className="font-bold text-xs">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} beds`
+                : `${listing.bedrooms} bed`}
+            </div>
+            <div className="font-bold text-xs">
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} baths`
+                : `${listing.bathrooms} bath`}
+            </div>
+          </div>
         </div>
       </Link>
     </div>
